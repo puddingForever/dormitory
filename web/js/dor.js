d37befc6bb4d3d6d082598698669898880cc8306
jje <developerjieun94@gmail.com>
2024-11-05 13:48:06 +0900

    git statu

--- web/js/dor.js
+++ web/js/dor.js
@@ -211,83 +211,22 @@ var dormitorys = [
   },
 ];
 
-/*
-@ Param rooms      the array of rooms
-@ Brief returns the floor with its rooms
-@ Date : 05/11/2024
-*/
-
-function checkFloor(rooms) {
-  var roomCountByFloor = {};
-  rooms.forEach((roomData) => {
-    var floor = Math.floor(roomData.room / 100); // floor
-    // if floor appears first, initialize it
-    if (!roomCountByFloor[floor]) {
-      roomCountByFloor[floor] = 0;
-    }
-    // {1: 5, 2: 5, 3: 4, 4: 1, 5: 5}
-    roomCountByFloor[floor]++;
-  });
-
-  return roomCountByFloor; // contains floor with number of its rooms,
-}
-
-/*
-@ Param rooms      the array of rooms
-       tableId     id of the table that are going to draw into  
-@ Brief draws dormitory .outer_table
-@ Date : 05/11/2024
-*/
-function drawDormitory(rooms, tableId) {
-  var table = document.querySelector('.' + tableId);
+var tower = document.querySelector('.selector_select');
+tower.addEventListener('change', function () {
+  var selectedTower = tower.value;
+  var table = document.querySelector('.outer_table');
   table.innerHTML = '';
-
-  // floor and its room count
-  var floorAndRoom = checkFloor(rooms);
-  // number of floors
-  var floorLength = Object.keys(floorAndRoom).length;
-  // maximum number of rooms
-  var maxRoomNumber = Math.max(...Object.values(floorAndRoom));
-
-  for (var i = floorLength - 1; i >= 0; i--) {
-    var row = table.insertRow();
-    row.classList.add('outer_table_row');
-    // current floor
-    var currentFloor = Object.keys(floorAndRoom)[i];
-
-    for (var j = 0; j < maxRoomNumber; j++) {
-      // caculate room number
-      var roomNumber = parseInt(currentFloor) * 100 + (j + 1);
-      // check if room exists
-      var roomInfo = rooms.find((room) => room.room === roomNumber);
-
-      if (roomInfo) {
-        var td = row.insertCell();
-        td.classList.add('outer_table_row_td');
-        var innerTable = document.createElement('table');
-        innerTable.classList.add('inner_table');
-        td.appendChild(innerTable);
-
-        // drawing rooms
-        var room_row = innerTable.insertRow();
-        var room_td = room_row.insertCell();
-        room_td.textContent = roomInfo.room;
-
-        var employee_row = innerTable.insertRow();
-        var employee_td = employee_row.insertCell();
-        employee_td.textContent = roomInfo.employee.join(', ');
-
-        var department_row = innerTable.insertRow();
-        var department_td = department_row.insertCell();
-        department_td.textContent = roomInfo.department.join(', ');
-      } else {
-        // drawing empty rooms
-        var td = row.insertCell();
-        td.classList.add('outer_table_row_td');
-        var innerTable = document.createElement('table');
-        innerTable.classList.add('empty_table');
-        td.appendChild(innerTable);
-      }
-    }
+  switch (selectedTower) {
+    case 'A': // tower A
+      var rooms = dormitorys[0].rooms;
+      drawDormitory(rooms, 'outer_table');
+      break;
+    case 'B': // tower B
+      var rooms = dormitorys[1].rooms;
+      drawDormitory(rooms, 'outer_table');
+      break;
+    default:
+      console.log('no data');
+      break;
   }
-}
+});
